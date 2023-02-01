78d6d459e9f644a80188c5e366f899baa7deaa86
intelligentdnb <juandiarts@gmail.com>
2023-02-01 12:49:00 -0300

    fix: console error /atom duplicate key/. fixed using RecoilEnv variable, waiting for new release of recoil

--- src/atoms/authModalAtom.ts
+++ src/atoms/authModalAtom.ts
@@ -1,17 +1,18 @@
- import { atom } from "recoil";
+import { atom } from "recoil";
+import {RecoilEnv} from "recoil";
+RecoilEnv.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED = false;
 
  export interface AuthModalState {
     open: boolean;
     view: "login" | "signup" | "resetPassword";
-
- }
+ };
 
  const defaultModalState: AuthModalState = {
     open: false,
     view: "login",
- }
+ };
 
  export const AuthModalState = atom<AuthModalState>({
     key: "authModalState",
     default: defaultModalState,
- })
+ });
