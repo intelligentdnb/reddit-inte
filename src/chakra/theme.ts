6b612504e5b1d730593588376739b752c9641d60
intelligentdnb <juandiarts@gmail.com>
2023-01-31 22:47:57 -0300

    feat/(src/chakra/): adding buttom.ts and CSS on theme

--- src/chakra/theme.ts
+++ src/chakra/theme.ts
@@ -3,12 +3,13 @@ import "@fontsource/open-sans/300.css";
 import "@fontsource/open-sans/400.css";
 import "@fontsource/open-sans/700.css";
 import { extendTheme } from "@chakra-ui/react";
+import { Button } from "./button";
 
 // 2. Call `extendTheme` and pass your custom values
 export const theme = extendTheme({
   colors: {
     brand: {
-      100: "#ff3c00",
+      100: "#FF3C00",
     },
   },
   fonts: {
@@ -22,6 +23,6 @@ export const theme = extendTheme({
     }),
   },
   components: {
-    // Button
+    Button
   }
 })
