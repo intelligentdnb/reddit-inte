cb071478514ace90d7e03257e27d988e2917e985
intelligentdnb <juandiarts@gmail.com>
2023-02-01 15:41:47 -0300

    feat: creating the auth inputs and oauth buttons component for the authModal

---
+++ src/components/Modal/Auth/AuthInputs.tsx
@@ -0,0 +1,20 @@
+import { AuthModalState } from '@/src/atoms/authModalAtom';
+import { useRecoilValue } from "recoil";
+import { Flex } from '@chakra-ui/react';
+import React from 'react';
+import LogIn from './LogIn';
+import SignUp from './SignUp';
+
+type AuthInputsProps = {};
+
+const AuthInputs:React.FC<AuthInputsProps> = () => {
+    const modalState = useRecoilValue(AuthModalState);
+
+    return (
+        <Flex>
+            {modalState.view === "login" && <LogIn />}
+            {modalState.view === "signup" && <SignUp />} 
+        </Flex>
+        )
+}
+export default AuthInputs;
