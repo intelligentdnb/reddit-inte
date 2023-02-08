ad00978fe76ac5e7ed6719bfddd0e26afe2ddf47
intelligentdnb <juandiarts@gmail.com>
2023-02-08 15:27:32 -0300

    feat: structure of community pages and input of new post

---
+++ src/components/Posts/TabItem.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { TabItem } from "./NewPostForm";
+
+type TabItemCompProps= {
+    item: TabItem;
+};
+
+const TabItemComp: React.FC<TabItemCompProps> = ({ item }) => {
+
+    return (
+        <div></div>
+    );
+};
+export default TabItemComp;
