9ef6fea37a5306692dc44a74cc13fe5d38d73dfc
intelligentdnb <juandiarts@gmail.com>
2023-02-16 16:58:10 -0300

    feat: tab items post varieties to create

--- src/components/Posts/TabItem.tsx
+++ src/components/Posts/TabItem.tsx
@@ -1,14 +1,24 @@
+import { Flex, Text, Icon } from "@chakra-ui/react";
 import React from "react";
 import { TabItem } from "./NewPostForm";
 
 type TabItemCompProps= {
     item: TabItem;
+    selected: boolean;
+    setSelectedTab: (value: string) => void;
 };
 
-const TabItemComp: React.FC<TabItemCompProps> = ({ item }) => {
+const TabItemComp: React.FC<TabItemCompProps> = ({ item, selected, setSelectedTab }) => {
 
     return (
-        <div></div>
+        <Flex justify="center" align="center" flexGrow={1} p="14px 0px" cursor="pointer" _hover={{bg: "gray.50"}} color={selected ? "blue.500" : "gray.500"}
+            borderWidth={selected ? "0px 1px 2px 0px" : "0px 1px 1px 0px"} borderBottomColor={selected ? "blue.500" : "gray.200"} borderRightColor="gray.200"
+            onClick={() => setSelectedTab(item.title)} fontWeight={700} >
+            <Flex align="center" height="20px" mr={2}>
+                <Icon as={item.icon}/>
+            </Flex>
+            <Text fontSize="10pt">{item.title}</Text>
+        </Flex>
     );
 };
 export default TabItemComp;
