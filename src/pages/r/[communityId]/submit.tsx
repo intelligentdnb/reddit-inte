2130d3a7dbe3e23fb66dc61435edc5514e5cff25
intelligentdnb <juandiarts@gmail.com>
2023-02-21 11:58:30 -0300

    feat: post single page created, state contexts ready to share individual links of posts, +About in submit/siglePagePost

--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -1,15 +1,17 @@
-import { communityState } from '@/src/atoms/communitiesAtom';
+import About from '@/src/components/Community/About';
 import PageContent from '@/src/components/Layout/PageContent';
 import NewPostForm from '@/src/components/Posts/NewPostForm';
 import { auth } from '@/src/firebase/clientApp';
+import useCommunityData from '@/src/hooks/useCommunityData';
 import { Box, Text } from '@chakra-ui/react';
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { useRecoilValue } from 'recoil';
+
 
 const SubmitPostPage: React.FC = () => {
     const [user] = useAuthState(auth);
-    const communityStateValue = useRecoilValue(communityState);
+    //const communityStateValue = useRecoilValue(communityState);
+    const { communityStateValue } = useCommunityData();
 
     return (
         <PageContent>
@@ -17,10 +19,12 @@ const SubmitPostPage: React.FC = () => {
                 <Box p="14px 0px" borderBottom="1px solid" borderColor="white">
                     <Text>Create a post</Text>
                 </Box>
-                {user && <NewPostForm user={user}/>}
+                {user && <NewPostForm user={user} />}
             </>
             <>
-            {/**About */}
+                {communityStateValue.currentCommunity && (
+                    <About communityData={communityStateValue.currentCommunity} />
+                )}
             </>
         </PageContent>
     )
