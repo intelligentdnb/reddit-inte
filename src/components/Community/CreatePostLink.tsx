358385c3089b095f97e387daa8e1e24660cd440b
intelligentdnb <juandiarts@gmail.com>
2023-02-22 01:26:12 -0300

    fix: when are in home open the communities directory for posting in some community

--- src/components/Community/CreatePostLink.tsx
+++ src/components/Community/CreatePostLink.tsx
@@ -8,11 +8,13 @@ import { IoImageOutline } from "react-icons/io5";
 import { useSetRecoilState } from "recoil";
 import { authModalState } from "@/src/atoms/authModalAtom";
 import { auth } from "@/src/firebase/clientApp";
+import useDirectory from "@/src/hooks/useDirectory";
 
 const CreatePostLink: React.FC = () => {
     const router = useRouter();
     const [user] = useAuthState(auth);
     const setAuthModalState = useSetRecoilState(authModalState);
+    const { toggleMenuOpen } = useDirectory()
 
     const onClick = () => {
         if (!user) {
@@ -20,7 +22,13 @@ const CreatePostLink: React.FC = () => {
             return;
         }
         const { communityId } = router.query;
-        router.push(`/r/${communityId}/submit`);
+
+        if(communityId) {
+            router.push(`/r/${communityId}/submit`);
+            return;
+        };
+        // open our directory menu
+        toggleMenuOpen();
     };
     return (
         <Flex
